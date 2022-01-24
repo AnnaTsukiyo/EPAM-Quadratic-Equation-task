512642389f69f70998062e034d1303c71c976e86
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-24 11:43:22 +0200

    done

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,22 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        double d = b * b - 4.0 * a * c;
+        if (d > 0) {
+            double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+            System.out.println(r2 + " " + r1);
 
+        } else if (d == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println(r1);
+        } else {
+            System.out.println("no roots");
+        }
+    }
+}
 
 
     }
 
-}
+            }
